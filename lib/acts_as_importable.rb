849191788f2e32cdc96b8e61daa7551608f96a7b
“Keith <kweightman@apangea.com>
2012-04-24 13:46:12 -0400

    Updated to be Rails 3.2-ified. Version bump

--- lib/acts_as_importable.rb
+++ lib/acts_as_importable.rb
@@ -1,90 +1,82 @@
-module Acts
-  module Importable
+module AMC
+  module Acts
+    module Importable
 
-    def self.included(base)
-      base.extend(ClassMethods)
-    end
-
-    module ClassMethods
-
-      def acts_as_importable(options = {})
-        # Store the import target class with the legacy class
-        write_inheritable_attribute :importable_to, options[:to]
-
-        # Don't extend or include twice. This will allow acts_as_importable to be called multiple times.
-        # eg. once in a parent class and once again in the child class, where it can override some options.
-        extend  Acts::Importable::SingletonMethods unless self.methods.include?('import') && self.methods.include?('import_all')
-        include Acts::Importable::InstanceMethods unless self.included_modules.include?(Acts::Importable::InstanceMethods)
+      def self.included(base)
+        base.extend(ClassMethods)
       end
 
-    end # ClassMethods
+      module ClassMethods
 
-    module SingletonMethods
-      def import(id)
-        find(id).import
-      end
+        def acts_as_importable(options = {})
+          # Store the import target class with the legacy class
+          #write_inheritable_attribute :importable_to, options[:to]
+          #method deprecated replaced with class_attribute
+          class_attribute :importable_to
+          self.importable_to = options[:to]
 
-      def import_all
-        self.before_import if self.method_defined? :before_import
-        new_models = all.collect do |legacy_model|
-          legacy_model.import
+          # Don't extend or include twice. This will allow acts_as_importable to be called multiple times.
+          # eg. once in a parent class and once again in the child class, where it can override some options.
+          extend  AMC::Acts::Importable::SingletonMethods unless self.methods.include?('import') && self.methods.include?('import_all')
+          include AMC::Acts::Importable::InstanceMethods unless self.included_modules.include?(AMC::Acts::Importable::InstanceMethods)
         end
-        self.after_import(new_models) if self.method_defined? :after_import
-      end
 
-      def import_all_in_batches
-        self.before_import if self.method_defined? :before_import
-        self.find_each do |legacy_model|
-          legacy_model.import
+      end # ClassMethods
+
+      module SingletonMethods
+        def import(id)
+          find(id).import
         end
-        self.after_import if self.method_defined? :after_import
-      end
 
-      def lookup_class
-        read_inheritable_attribute(:importable_to) || "#{self.to_s.split('::').last}"
-      end
+        def import_all(*args)
+          all(*args).each do |legacy_model|
+            legacy_model.import
+          end
+        end
 
-      def lookup(id)
-        lookups[id] ||= lookup_class.constantize.first(:conditions => {:legacy_id => id, :legacy_class => self.to_s}).try(:id)
-      end
+        # This requires a numeric primary key for the legacy tables
+        def import_all_in_batches(*args)
+          find_each(*args) do |legacy_model|
+            legacy_model.import
+          end
+        end
 
-      def flush_lookups!
-        @lookups = {}
-      end
+        def lookup(id)
+          #lookup_class = read_inheritable_attribute(:importable_to) || "#{self.to_s.split('::').last}"
+          lookup_class = self.importable_to || "#{self.to_s.split('::').last}"
+          lookups[id] ||= Kernel.const_get(lookup_class).first(:conditions => {:legacy_id => id, :legacy_class => self.to_s}).try(:id)
+        end
 
-      private
+        def flush_lookups!
+          @lookups = {}
+        end
 
-      def lookups
-        @lookups ||= {}
-      end
+        private
 
-    end # SingletonMethods
+        def lookups
+          @lookups ||= {}
+        end
 
-    module InstanceMethods
+      end # SingletonMethods
 
-      def import
-        before_import if self.class.instance_methods.include? :before_import
-        to_model.tap do |new_model|
-          if new_model
-            new_model.legacy_id     = self.id         if new_model.respond_to?(:"legacy_id=")
-            new_model.legacy_class  = self.class.to_s if new_model.respond_to?(:"legacy_class=")
+      module InstanceMethods
 
-            if !new_model.save(:validate => false)
-              p new_model.errors
-              # TODO log an error that the model failed to save
-              # TODO remove the raise once we're out of the development cycle
-              raise
-            else
-              after_import(new_model) if self.class.instance_methods.include? :after_import
+        def import
+          to_model.tap do |new_model|
+            if new_model
+              new_model.legacy_id     = self.id         if new_model.respond_to?(:"legacy_id=")
+              new_model.legacy_class  = self.class.to_s if new_model.respond_to?(:"legacy_class=")
+
+              if !new_model.save
+                puts "#{new_model.errors.full_messages} <#{self.class.name}:#{id}>"
+              end
             end
           end
         end
-      end
-    end # InstanceMethods
+      end # InstanceMethods
 
+    end
   end
 end
 
-require 'core_extensions'
-ActiveRecord::Base.class_eval { include Acts::Importable }
-
+ActiveRecord::Base.class_eval { include AMC::Acts::Importable }
